0819c83361a587073459b6f8c607bd3e75b7a81a
BellaBradbury <bellabradbury@yahoo.com>
2023-06-20 15:28:01 -0400

    js picture spin

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-// console.log('APP ATTACHED');
+// PICTURE SPIN
+let techCot = document.getElementsByClassName('about-tech')[0];
+let techItmArr = Array.from(techCot.children);
+
+techItmArr.forEach(itm => {
+    itm.addEventListener('mouseover', (e)=> {
+        itm.style.transform = "rotate(360deg) scale(1.5)";
+    });
+    itm.addEventListener('mouseout', (e)=> {
+        itm.style.transform = "scale(1)";
+    })
+});
